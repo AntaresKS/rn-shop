fce67aa28a41277e13c55e99d5ca207704ed9d44
AntaresKS <101465248+AntaresKS@users.noreply.github.com>
2024-11-29 21:25:39 +0100

    add some style

--- assets/products.ts
+++ assets/products.ts
@@ -23,3 +23,35 @@ export const PRODUCTS: Product[] = [
     id: 5,
     title: "Dell XPS 13",
     slug: "dell-xps-13",
+    heroImage: require("../assets/images/dell-1.jpg"),
+    imageUrl: [
+      require("../assets/images/dell-1.jpg"),
+      require("../assets/images/dell-2.jpg"),
+    ],
+    price: 1099.99,
+    category: {
+      imageUrl: require("../assets/images/dell-1.jpg"),
+      name: "Laptops",
+      slug: "laptops",
+    },
+    maxQuantity: 7,
+  },
+  {
+    id: 2,
+    title: "iPhone 16 Pro Max",
+    slug: "iphone-16-pro-max",
+    heroImage: require("../assets/images/i-phone-1.jpg"),
+    imageUrl: [
+      require("../assets/images/i-phone-1.jpg"),
+      require("../assets/images/i-phone-2.jpg"),
+      require("../assets/images/i-phone-3.jpg"),
+    ],
+    price: 999.99,
+    category: {
+      imageUrl: require("../assets/images/i-phone-1.jpg"),
+      name: "Phones",
+      slug: "phones",
+    },
+    maxQuantity: 5,
+  },
+];
